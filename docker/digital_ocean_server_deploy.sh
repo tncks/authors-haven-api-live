fb5c29506126332262ac8896649afb6091845bc7
tncks <26240553+tncks@users.noreply.github.com>
2022-04-07 10:35:30 +0900

    setupt done

--- docker/digital_ocean_server_deploy.sh
+++ docker/digital_ocean_server_deploy.sh
@@ -1,20 +1,16 @@
 #! /bin/bash
 
-if [ -z "$DIGITAL_OCEAN_IP_ADDRESS" ]
-then
-    echo "DIGITAL_OCEAN_IP_ADDRESS not defined"
-    exit 0
-fi
+
 
 git archive --format tar --output ./project.tar main
 
 echo "Uploading the project.....:-)...Be Patient!"
-rsync ./project.tar root@$DIGITAL_OCEAN_IP_ADDRESS:/tmp/project.tar
+rsync ./project.tar root@134.209.181.179:/tmp/project.tar
 echo "Upload complete....:-)"
 
 
 echo "Building the image......."
-ssh -o StrictHostKeyChecking=no root@$DIGITAL_OCEAN_IP_ADDRESS << 'ENDSSH'
+ssh -o StrictHostKeyChecking=no root@134.209.181.179 << 'ENDSSH'
     mkdir -p /app
     rm -rf /app/* && tar -xf /tmp/project.tar -C /app
     docker-compose -f /app/production.yml build
