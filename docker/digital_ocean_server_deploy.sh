2aa3b04f7f9ebb17f8969bcc7779389476bc30b7
tncks <26240553+tncks@users.noreply.github.com>
2022-04-07 13:05:35 +0900

    local development sources finally released

--- docker/digital_ocean_server_deploy.sh
+++ docker/digital_ocean_server_deploy.sh
@@ -5,13 +5,14 @@
 git archive --format tar --output ./project.tar master
 
 echo "Uploading the project.....:-)...Be Patient!"
-rsync ./project.tar root@134.209.181.179:/tmp/project.tar
-echo "Upload complete....:-)"
+
+
+
 
 
 echo "Building the image......."
 ssh -o StrictHostKeyChecking=no root@134.209.181.179 << 'ENDSSH'
-    mkdir -p /app
+    mkdir -p /app && rsync ./project.tar root@134.209.181.179:/tmp/project.tar
     rm -rf /app/* && tar -xf /tmp/project.tar -C /app
     docker-compose -f /app/production.yml build
 
